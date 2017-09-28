bdc7c56ed6b4e783380ba6cdea72ee38aa80aa58
Renato  Cavalcanti <renato@lightbend.com>
2017-09-28 14:55:46 +0200

    add explicit types for command and event handlers

--- src/main/scala/com/lightbend/lagom/core/persistence/effects/core/persistence/AccountEntity.scala
+++ src/main/scala/com/lightbend/lagom/core/persistence/effects/core/persistence/AccountEntity.scala
@@ -37,7 +37,7 @@ class AccountEntity extends PersistentEntity[AccountCommand[_], AccountEvent, Ac
 
 
   // on creation and later
-  val depositCommandHandlers =
+  val depositCommandHandlers: CommandHandlers =
     onCommand {
       // this is directive builder that expects one single Event
       Handler[Deposit]
@@ -51,7 +51,7 @@ class AccountEntity extends PersistentEntity[AccountCommand[_], AccountEvent, Ac
         .replyWith(_.amount)
     }
 
-  def withdrawCommandHandlers(account: Account) =
+  def withdrawCommandHandlers(account: Account): CommandHandlers =
     onCommand {
       Handler[Withdraw]
         .attempt.persistOne { // <- this Effect builder expects a Try[Event]
@@ -68,7 +68,7 @@ class AccountEntity extends PersistentEntity[AccountCommand[_], AccountEvent, Ac
     * behind the scenes, this is a regular Effect with a NoOps command handler.
     * It doesn't have callbacks neither, but do have a reply.
     */
-  val readOnlyCommandHandlers =
+  val readOnlyCommandHandlers: CommandHandlers =
     onCommand {
       ReadOnly[GetBalance.type].replyWith(_.amount)
     }
@@ -78,13 +78,13 @@ class AccountEntity extends PersistentEntity[AccountCommand[_], AccountEvent, Ac
       ReadOnly[GetState.type].replyWith(identity)
     }
 
-  val atCreationEventsHandlers =
+  val atCreationEventsHandlers: EventHandlers =
     onEvent {
       // at creation time, first DepositExecuted initialises the account
       case evt: DepositExecuted => Account(evt.amount)
     }
 
-  def afterCreationEventsHandlers(account: Account) =
+  def afterCreationEventsHandlers(account: Account): EventHandlers =
     onEvent {
       case evt: DepositExecuted => account.copy(amount = account.amount + evt.amount)
       case evt: WithdrawExecuted => account.copy(amount = account.amount - evt.amount)
