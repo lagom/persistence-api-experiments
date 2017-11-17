dd055241b47e4c36fc0e8dd750136ed1bcad4c35
Renato  Cavalcanti <renato@lightbend.com>
2017-11-17 02:12:13 +0100

    moving Raffle trait to inside PersistentEntity

--- src/test/scala/com/lightbend/lagom/raffle/model/Raffle.scala
+++ src/test/scala/com/lightbend/lagom/raffle/model/Raffle.scala
@@ -32,99 +32,98 @@ object Raffle extends PersistentEntity {
         case FinishedRaffle =>
           FinishedRaffle.rejectAllCommands
       }
-}
+      
+    sealed trait Raffle
 
-import com.lightbend.lagom.raffle.model.Raffle._
+    case object EmptyRaffle extends Raffle {
 
-sealed trait Raffle
-
-case object EmptyRaffle extends Raffle {
-
-
-  val acceptParticipants =
-    actions
-      .onCommand[AddParticipant] {
-        case AddParticipant(name) =>
-          Effect.persist(ParticipantAdded(name))
-      }
-      .onEvent {
-        case ParticipantAdded(name) => NonEmptyRaffle(List(name))
-      }
-
-  val canNotRunWithoutParticipants =
-    actions
-      .rejectCommand[Run.type]("Raffle has no participants")
-
-  val ignoreRemovals =
-    actions
-      .onCommand[RemoveAllParticipants.type] {
-        case _ => Effect.ignore
-      }
-      .onCommand[RemoveParticipant] {
-        case _ => Effect.ignore
-      }
-}
-
-case class NonEmptyRaffle(participants: List[String]) extends Raffle {
-
-  def hasParticipant(name: String) = participants.contains(name)
-
-  val acceptParticipants =
-    actions
-      .onCommand[AddParticipant] {
-        // reject double booking
-        case AddParticipant(name) if hasParticipant(name) =>
-          Effect.reject(s"""Participant $name already added!""" )
-
-        case AddParticipant(name) =>
-          Effect.persist(ParticipantAdded(name))
-      }
-      .onEvent {
-        case ParticipantAdded(name) => copy(participants = participants :+ name)
-      }
-
-
-  val removeParticipants =
-    actions
-      .onCommand[RemoveParticipant] {
-        case RemoveParticipant(name) =>
-          Effect.persist {
-            participants.find(_ == name).map(ParticipantRemoved)
+      val acceptParticipants =
+        actions
+          .onCommand[AddParticipant] {
+            case AddParticipant(name) =>
+              Effect.persist(ParticipantAdded(name))
           }
-      }
-      .onCommand[RemoveAllParticipants.type] {
-        case RemoveAllParticipants =>
-          Effect.persist {
-            participants.map(ParticipantRemoved)
+          .onEvent {
+            case ParticipantAdded(name) => NonEmptyRaffle(List(name))
           }
-      }
-      .onEvent {
-        case ParticipantRemoved(name) =>
-          val remaining = participants.filter(_ != name)
-          // NOTE: if last participant is removed, transition back to EmptyRaffle
-          if (remaining.isEmpty)
-            EmptyRaffle
-          else
-            copy(participants = remaining)
-      }
 
-  val run =
-    actions
-      .onCommand[Run.type] {
-        case Run =>
-          val index  = Random.nextInt(participants.size)
-          val winner = participants(index)
-          Effect.persist(WinnerSelected(winner, OffsetDateTime.now))
-      }
-     .onEvent {
-       case _: WinnerSelected => FinishedRaffle
-     }
+      val canNotRunWithoutParticipants =
+        actions
+          .rejectCommand[Run.type]("Raffle has no participants")
 
+      val ignoreRemovals =
+        actions
+          .onCommand[RemoveAllParticipants.type] {
+            case _ => Effect.ignore
+          }
+          .onCommand[RemoveParticipant] {
+            case _ => Effect.ignore
+          }
+    }
+
+    case class NonEmptyRaffle(participants: List[String]) extends Raffle {
+
+      def hasParticipant(name: String) = participants.contains(name)
+
+      val acceptParticipants =
+        actions
+          .onCommand[AddParticipant] {
+            // reject double booking
+            case AddParticipant(name) if hasParticipant(name) =>
+              Effect.reject(s"""Participant $name already added!""" )
+
+            case AddParticipant(name) =>
+              Effect.persist(ParticipantAdded(name))
+          }
+          .onEvent {
+            case ParticipantAdded(name) => copy(participants = participants :+ name)
+          }
+
+
+      val removeParticipants =
+        actions
+          .onCommand[RemoveParticipant] {
+            case RemoveParticipant(name) =>
+              Effect.persist {
+                participants.find(_ == name).map(ParticipantRemoved)
+              }
+          }
+          .onCommand[RemoveAllParticipants.type] {
+            case RemoveAllParticipants =>
+              Effect.persist {
+                participants.map(ParticipantRemoved)
+              }
+          }
+          .onEvent {
+            case ParticipantRemoved(name) =>
+              val remaining = participants.filter(_ != name)
+              // NOTE: if last participant is removed, transition back to EmptyRaffle
+              if (remaining.isEmpty)
+                EmptyRaffle
+              else
+                copy(participants = remaining)
+          }
+
+      val run =
+        actions
+          .onCommand[Run.type] {
+            case Run =>
+              val index  = Random.nextInt(participants.size)
+              val winner = participants(index)
+              Effect.persist(WinnerSelected(winner, OffsetDateTime.now))
+          }
+         .onEvent {
+           case _: WinnerSelected => FinishedRaffle
+         }
+
+    }
+
+    case object FinishedRaffle extends Raffle {
+      def rejectAllCommands = actions.rejectAll("Raffle is already finished")
+    }
 }
 
-case object FinishedRaffle extends Raffle {
-  def rejectAllCommands = actions.rejectAll("Raffle is already finished")
-}
+
 
 sealed trait RaffleCommand extends ReplyType[Done]
 
